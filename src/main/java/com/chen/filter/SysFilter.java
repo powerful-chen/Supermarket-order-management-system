ee4772b7c127b57fd6c9782c0fc75910a408e490
小陈 <1716867307@qq.com>
2020-08-03 21:04:59 +0800

    注销登录和优化

---
+++ src/main/java/com/chen/filter/SysFilter.java
@@ -0,0 +1,42 @@
+package com.chen.filter;
+
+import com.chen.pojo.User;
+import com.chen.util.Constants;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @ClassName SysFilter
+ * @Description TODO
+ * @Author xiaochen
+ * @Date 2020/8/3 20:46
+ */
+public class SysFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        //过滤器，从Session中获取用户
+        User user = (User) request.getSession().getAttribute(Constants.USER_SESSION);
+
+        if (user == null) {//已经被移除或者注销了，或者未登录
+            response.sendRedirect("/smbms/error.jsp");
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/main/java/com/chen/servlet/user/LogoutServlet.java
@@ -0,0 +1,29 @@
+package com.chen.servlet.user;
+
+import com.chen.util.Constants;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @ClassName LogoutServlet
+ * @Description TODO
+ * @Author xiaochen
+ * @Date 2020/8/3 20:34
+ */
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //移除用户的Constants.USER_SESSION
+        req.getSession().removeAttribute(Constants.USER_SESSION);
+        resp.sendRedirect(req.getContextPath()+"/login.jsp");//返回登录页面
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
